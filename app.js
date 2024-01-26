cccb7835d7275c09e8d0cb5b38a896d3d4e7b44f
synoap <devsynoap@gmail.com>
2024-01-26 16:33:41 -0300

    new feat: speak and another things

--- app.js
+++ app.js
@@ -1,78 +1,69 @@
-// Definindo variáveis
-alert('Boas vindas ao jogo do número secreto');
+// Nossas variaveis
+let listaNumerosSorteados = [];
+let numLimite = 10
+let numeroSecreto = gerarNumeroAleatorio();
+var numTentativas = 1;
 
-// Entrada de dados do usuario
-let nome = prompt('Qual seu nome?');
-alert(`Olá, ${nome}!`);
-let idade = prompt('Qual sua idade?');
-console.log(`voce tem ${idade} anos de idade.`);
+// Input [0-9] apenas
+document.querySelector('.container__input').addEventListener("keyup", function() {
+  this.value = this.value.replace(/[^0-9]/g, "");
+});
 
-if (idade >= 18) {
-  console.log('Voce tem idade e já pode jogar!,');
-} else {
-  console.log('Voce não tem idade e não pode jogar!');
+function exibirTextoNaTela(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
+  responsiveVoice.speak(texto, 'Brazilian Portuguese Female', 
+  {rate: 1.2});
 }
 
-// Verifique se o numero é positivo ou negativo
-let numero = parseInt(prompt('Digite um número:'));
-if (numero > 0) {
-  console.log(`O número ${numero} é positivo`);
-} else if (numero < 0) {
-  console.log(`O número ${numero} é negativo`);
-} else {
-  console.log('O número é zero');
+function exibirMsgInitial() {
+  exibirTextoNaTela('h1', 'Jogo do Número Secreto!');
+  exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 }
 
-// loop imprime de 1 a 10
-let contador = 1;
+function gerarNumeroAleatorio() {
+  let numeroEscolhido = parseInt(Math.random() * numLimite + 1);
+  let qtdDeObjectsNaLista = listaNumerosSorteados.length;
 
-while(contador < 10 + 1) {
-  console.log(`imprimindo ${contador}`);
-  contador++;
-}
-
-let nota = parseInt(prompt('Qual sua nota?'));
-let seNota = nota >= 7 ? 'aprovada' : 'reprovada';
-console.log(`Sua nota foi ${seNota}`);
-
-// Linguagem de programação
-let linguagem = prompt('Qual linguagem de programação você mais gosta?');
-console.log(`A linguagem ${linguagem} é muito legal!`);
-
-// subtração
-let valor1 = parseInt(prompt('Escolha um número.'));
-let valor2 = parseInt(prompt('Escolha outro número.'));
-let resultado = valor1 - valor2;
-console.log(`A subtração de ${valor1} - ${valor2} é ${resultado}.`);
-
-// numero secreto
-let = numMax = 1000
-const numeroSecreto = parseInt(Math.random() * numMax + 1);
-let chute;
-let tentativas = 1;
-
-// Laço de repetição (while)
-while(chute != numeroSecreto){
-  chute = prompt(`Escolha um número entre 1 e ${numMax}`);
-  if(chute == numeroSecreto){
-    break;
+  if(qtdDeObjectsNaLista == numLimite) {
+    listaNumerosSorteados = [];
+  }
+  
+  if(listaNumerosSorteados.includes(numeroEscolhido)) {
+    return gerarNumeroAleatorio();
   } else {
-    if(chute > numeroSecreto){
-      alert(`O número secreto é menor que ${chute}`)
-    } else {
-      alert(`O número secreto é maior ${chute}`)
-    }
-    tentativas++
+    listaNumerosSorteados.push(numeroEscolhido);
+    console.log(listaNumerosSorteados)
+    return numeroEscolhido;
   }
 }
 
-// Operador ternário para o plural da palavra tentativa.
-const palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
-alert(`O número secreto era ${numeroSecreto}! E você acertou com apenas ${tentativas} ${palavraTentativa}`)
+function verificarChute() {
+  let palavaTentativa = numTentativas > 1 ? 'tentativas' : 'tentativa';
+  let chute = document.querySelector('.container__input').value;
+  let msgTentativas = `Você acertou o número secreto com ${numTentativas} ${palavaTentativa}!`;
+  if(chute > numeroSecreto) {
+    exibirTextoNaTela('p', `O número secreto é menor que: ${chute}`);
+  } else if(chute < numeroSecreto) {
+    exibirTextoNaTela('p', `O número secreto é maior que: ${chute}`);
+  } else {
+    exibirTextoNaTela('h1', 'Você acertou!');
+    exibirTextoNaTela('p', msgTentativas);
+    document.getElementById('reiniciar').removeAttribute('disabled');
+  }
+  numTentativas++;
+  limparInput();
+}
 
-// numero aleatorio
-let numeroAleatorio = parseInt(Math.random() * 10 + 1);
-console.log(`O número aleatório é ${numeroAleatorio}`)
+function limparInput() {
+  chute = document.querySelector('.container__input');
+  chute.value = '';
+}
 
-let numAleatorioMil = parseInt(Math.random() * 1000 + 1);
-console.log(`O número aleatóriode 1 a 1000 é ${numAleatorioMil}`)
+function reiniciarJogo() {
+  numeroSecreto = gerarNumeroAleatorio();
+  limparInput();
+  numTentativas = 1;
+  exibirMsgInitial();
+  document.getElementById('reiniciar').setAttribute('disabled', true);
+} exibirMsgInitial();
